b4c1bb70f94b101c995539e152fc440ecaccef08
sorex <sorex1984@gmail.com>
2013-05-17 23:47:40 +0800

    日常提交

---
+++ J.BusinessLogics/Basic/SessionConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J.BusinessLogics.Basic
+{
+	public class SessionConfig
+	{
+		/// <summary>
+		/// 当前用户
+		/// </summary>
+		public const string CurrentUser = "CurrentUser";
+	}
+}
