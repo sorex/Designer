ffa99f0a21d6d84676e716f5c172f81eb471bdd0
sorex <sorex1984@gmail.com>
2013-07-22 17:18:54 +0800

    调整buy和preview页面的位置

--- J.MainWeb/Controllers/LogicController.cs
+++ J.MainWeb/Controllers/LogicController.cs
@@ -1,4 +1,5 @@
-﻿using J.Entities;
+﻿using J.BusinessLogics.Basic;
+using J.Entities;
 using J.MainWeb.App_Code;
 using J.Utility;
 using Newtonsoft.Json;
@@ -12,6 +13,101 @@ namespace J.MainWeb.Controllers
 {
 	public class LogicController : BaseController
 	{
+		#region 预览并发布
+		public ActionResult Preview(string guid)
+		{
+			guid = guid.ToLower();
+			using (DBEntities db = new DBEntities())
+			{
+				var dw = db.designworks.FirstOrDefault(p => p.GUID == guid);
+				if (dw == null)
+					return RedirectToAction("Error", "Home", new { msg = "该活动的预览不存在!" });
+
+				ViewBag.Title = dw.Title;
+
+				ViewBag.Data = JsonConvert.SerializeObject(new
+				{
+					DesignworkID = dw.GUID,
+					UserEmail = dw.user.Email,
+					Description = dw.Description,
+					MaterialDescription = dw.material.Description,
+					CurrentMaterialTypeID = dw.material.TypeID,
+					CurrentMaterialID = dw.MaterialID,
+					CurrentColorCode = dw.materialcolor.ColorCode
+				});
+
+				Dictionary<string, List<string>> TypeAndProperties = new Dictionary<string, List<string>>();
+				TypeAndProperties.Add("materialpicture", new List<string> { "GUID", "MaterialID", "Name", "Index", "FileName", "Width", "Height", "Top", "Left", "UploadWidth", "UploadHeight", "ShowScale" });
+
+				ViewBag.DataMaterialpictures = JsonConvert.SerializeObject(dw.material.materialpictures, new JsonSerializerSettings { ContractResolver = new DynamicContractResolver(TypeAndProperties) });
+			}
+			return View();
+		}
+
+		[HttpPost]
+		[ActionName("Preview")]
+		public ActionResult PreviewPost(string guid)
+		{
+			var UserID = base.CurrentUser.GUID;
+			guid = guid.ToLower();
+			using (DBEntities db = new DBEntities())
+			{
+				var dw = db.designworks.FirstOrDefault(p => p.GUID == guid && p.DesignerID == UserID);
+				if (dw == null)
+					return RedirectToAction("Error", "Home", new { msg = "该活动的预览不存在!" });
+				if (dw.State != 0)
+					return RedirectToAction("Error", "Home", new { msg = "该活动不能发布!" });
+
+				dw.State = 1;
+				var time = dw.EndTime - dw.StartTime;
+				dw.StartTime = DateTime.Now;
+				dw.EndTime = dw.StartTime.AddMilliseconds(time.TotalMilliseconds);
+				db.SaveChanges();
+			}
+			return Content(JsonConvert.SerializeObject(new { code = 1, msg = guid }));
+		}
+		#endregion
+
+		#region 购买
+		public ActionResult Buy(string guid)
+		{
+			guid = guid.ToLower();
+			using (DBEntities db = new DBEntities())
+			{
+				var dw = db.designworks.FirstOrDefault(p => p.GUID == guid);
+				if (dw == null)
+					return RedirectToAction("Error", "Home", new { msg = "该活动的预览不存在!" });
+
+				ViewBag.Title = dw.Title;
+
+				ViewBag.Data = JsonConvert.SerializeObject(new
+				{
+					DesignworkID = dw.GUID,
+					UserEmail = dw.user.Email,
+					Description = dw.Description,
+					MaterialDescription = dw.material.Description,
+					CurrentMaterialTypeID = dw.material.TypeID,
+					CurrentMaterialID = dw.MaterialID,
+					CurrentColorCode = dw.materialcolor.ColorCode
+				});
+
+				Dictionary<string, List<string>> TypeAndProperties = new Dictionary<string, List<string>>();
+				TypeAndProperties.Add("materialpicture", new List<string> { "GUID", "MaterialID", "Name", "Index", "FileName", "Width", "Height", "Top", "Left", "UploadWidth", "UploadHeight", "ShowScale" });
+				ViewBag.DataMaterialpictures = JsonConvert.SerializeObject(dw.material.materialpictures, new JsonSerializerSettings { ContractResolver = new DynamicContractResolver(TypeAndProperties) });
+
+				Dictionary<string, List<string>> TypeAndProperties2 = new Dictionary<string, List<string>>();
+				TypeAndProperties2.Add("designwork", new List<string> { "GUID", "DesignerID", "MaterialID", "MaterialColorID", "SalesGoal", "BasePrice", "SellingPrice", "StartTime", "EndTime", "SalesVolume", "State" });
+				ViewBag.DataDesignwork = JsonConvert.SerializeObject(dw, new JsonSerializerSettings { ContractResolver = new DynamicContractResolver(TypeAndProperties2) });
+
+				Dictionary<string, List<string>> TypeAndProperties3 = new Dictionary<string, List<string>>();
+				TypeAndProperties3.Add("materialsize", new List<string> { "GUID", "MaterialID", "SizeName", "Index" });
+				ViewBag.DataSize = JsonConvert.SerializeObject(dw.material.materialsizes.OrderBy(p => p.Index), new JsonSerializerSettings { ContractResolver = new DynamicContractResolver(TypeAndProperties3) });
+			}
+			return View();
+		}
+		#endregion
+
+		#region 创建订单
 		[HttpPost]
 		public ActionResult CreateOrder(string guid, string sizes, string quantities)
 		{
@@ -27,16 +123,19 @@ namespace J.MainWeb.Controllers
 			var quantitiesStr = sizes.Trim(',').ToLower().Split(',');
 			for (int i = 0; i < sizesStr.Length; i++)
 			{
-				sizequantity.Add(sizesStr[i], int.Parse(quantitiesStr[i]));
+				int tempQuantity = 0;
+				if (!int.TryParse(quantitiesStr[i], out tempQuantity))
+					return Content(JsonConvert.SerializeObject(new { code = -1, msg = "数据错误，请重试！" }));
+				
+				sizequantity.Add(sizesStr[i], tempQuantity);
 			}
 
 			using(DBEntities db = new DBEntities())
 			{
 				var DesignWork = db.designworks.FirstOrDefault(p=>p.GUID == DesignWorkID);
-
-				/*
-				 未效验输入数据的正确性和有效性
-				 */
+				var sizesDB = db.materialsizes.Where(p => sizesStr.Contains(p.GUID)).ToList();
+				if(sizesDB.Count() != sizesStr.Count())
+					return Content(JsonConvert.SerializeObject(new { code = -1, msg = "选择的尺寸不存在，请重试！" }));
 
 				order Order = new order()
 				{
@@ -54,16 +153,22 @@ namespace J.MainWeb.Controllers
 				
 				foreach (var s in sizequantity)
 				{
-					//未完成
-					var OrderDetail = new orderdetail() {
+					var OrderDetail = new orderdetail()
+					{
 						GUID = Basic.NewGuid(),
 						OrderID = OrderID,
-						SizeID = s.Key
+						SizeID = s.Key,
+						SizeName = sizesDB.FirstOrDefault(p => p.GUID == s.Key).SizeName,
+						Number = s.Value
 					};
+					db.orderdetails.Add(OrderDetail);
 				}
+
+				db.SaveChanges();
 			}
 
 			return Content(JsonConvert.SerializeObject(new { code = 1, msg = OrderID }));
 		}
+		#endregion
 	}
 }
