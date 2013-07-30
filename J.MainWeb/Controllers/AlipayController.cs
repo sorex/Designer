f97482932ad2b42d6919e6cd0aafdaf6b4d5d77b
sorex <sorex1984@gmail.com>
2013-07-30 23:54:09 +0800

    加入了Alipay的App_Code中的文件

---
+++ J.MainWeb/Controllers/AlipayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace J.MainWeb.Controllers
+{
+    public class AlipayController : Controller
+    {
+        //
+        // GET: /Alipay/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		//public ActionResult
+    }
+}
