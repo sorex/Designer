8aa1853ad60140718a02f8f756f56c5cfc9f1d6f
sorex <sorex1984@gmail.com>
2013-09-04 17:24:30 +0800

    日常提交

--- J.MainWeb/Controllers/AlipayController.cs
+++ J.MainWeb/Controllers/AlipayController.cs
@@ -338,7 +338,7 @@ namespace J.MainWeb.Controllers
 			//需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
 
 			//卖家支付宝帐户
-			string seller_email = System.Configuration.ConfigurationManager.AppSettings["SellerEmail"];
+			string seller_email = Config.Seller_email;
 			//必填
 
 			//商户订单号
@@ -431,6 +431,94 @@ namespace J.MainWeb.Controllers
 
 		}
 
+		public ActionResult Alipay_Pay2(string OrderID)
+		{
+			order Order = null;
+			string UserID = base.CurrentUser.GUID;
+			using (DBEntities db = new DBEntities())
+			{
+				Order = db.orders.FirstOrDefault(p => p.GUID == OrderID && p.UserID == UserID && p.State == 1);
+			}
+
+			if (Order == null)
+				return RedirectToAction("Error", "Home", new { Message = "订单不存在，或者已超时！" });
+
+
+			////////////////////////////////////////////请求参数////////////////////////////////////////////
+
+			//支付类型
+			string payment_type = "1";
+			//必填，不能修改
+			//服务器异步通知页面路径
+			string notify_url = "http://www.xxx.com/create_direct_pay_by_user-CSHARP-UTF-8/notify_url.aspx";
+			//需http://格式的完整路径，不能加?id=123这类自定义参数
+
+			//页面跳转同步通知页面路径
+			string return_url = "http://www.xxx.com/create_direct_pay_by_user-CSHARP-UTF-8/return_url.aspx";
+			//需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
+
+			//卖家支付宝帐户
+			string seller_email = Config.Seller_email;
+			//必填
+
+			//商户订单号
+			string out_trade_no = Order.GUID;
+			//商户网站订单系统中唯一订单号，必填
+
+			//订单名称
+			string subject = Order.Subject;
+			//必填
+
+			//付款金额
+			string total_fee = Order.Total.ToString();
+			//必填
+
+			//订单描述
+
+			string body = Order.Body;
+			//商品展示地址
+			string show_url = System.Configuration.ConfigurationManager.AppSettings["WebUrl"] + "Logic/Buy?guid=" + Order.DesignWorkID;
+			//需以http://开头的完整路径，例如：http://www.xxx.com/myorder.html
+
+			//防钓鱼时间戳
+			string anti_phishing_key = "";
+			//若要使用请调用类文件submit中的query_timestamp函数
+
+			//客户端的IP地址
+			string exter_invoke_ip = "";
+			//非局域网的外网IP地址，如：221.0.0.1
+
+			//共用回传参数
+			string extra_common_param = "";
+			//如果用户请求时传递了该参数，则返回给商户时会回传该参数
+
+
+			////////////////////////////////////////////////////////////////////////////////////////////////
+
+			//把请求参数打包成数组
+			SortedDictionary<string, string> sParaTemp = new SortedDictionary<string, string>();
+			sParaTemp.Add("partner", Config.Partner);
+			sParaTemp.Add("_input_charset", Config.Input_charset.ToLower());
+			sParaTemp.Add("service", "create_direct_pay_by_user");
+			sParaTemp.Add("payment_type", payment_type);
+			sParaTemp.Add("notify_url", notify_url);
+			sParaTemp.Add("return_url", return_url);
+			sParaTemp.Add("seller_email", seller_email);
+			sParaTemp.Add("out_trade_no", out_trade_no);
+			sParaTemp.Add("subject", subject);
+			sParaTemp.Add("total_fee", total_fee);
+			sParaTemp.Add("body", body);
+			sParaTemp.Add("show_url", show_url);
+			sParaTemp.Add("anti_phishing_key", anti_phishing_key);
+			sParaTemp.Add("exter_invoke_ip", exter_invoke_ip);
+			sParaTemp.Add("extra_common_param", extra_common_param);
+
+			//建立请求
+			string sHtmlText = Submit.BuildRequest(sParaTemp, "get", "确认");
+			return Content(sHtmlText);
+
+		}
+
 		public bool Alipay_PayOK(string PayTradeNO, string BuyerID, string OrderID)
 		{
 			var Result = false;
@@ -449,6 +537,10 @@ namespace J.MainWeb.Controllers
 			}
 			return Result;
 		}
+		#endregion
+
+		#region Post
+
 		#endregion
 	}
 }
